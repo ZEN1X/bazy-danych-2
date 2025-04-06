fe60d8b1dae6f5f12827e8f45131114498b8cf95
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2025-04-07 00:57:55 +0200

    feat: add solution for task 1 lab 6

---
+++ lab6/lab6_task1.sql
@@ -0,0 +1,55 @@
+-- Zadanie 1
+
+/*
+ Opracować skrypt T-SQL tworzący użytkownika Lab6user w bazie SQL Server (jeżeli
+ istnieje usunąć), bazę danych Lab6db (jeżeli istnieje usunąć) oraz na koniec przypisać
+ użytkownika Lab6user jako db_owner do bazy danych Lab6db.
+ */
+
+use [master];
+go
+
+-- tworzymy nowy login
+if exists ( select name
+            from sys.database_principals
+            where name = 'Lab6user' )
+    begin
+        drop login [Lab6user];
+    end
+go
+
+create login [Lab6user] with password = 'Passw0rd';
+go
+
+-- tworzymy nową bazę danych
+if exists ( select name
+            from sys.databases
+            where name = 'Lab6db' )
+    begin
+        -- dla bezpieczeństwa rozłączmy wszystkich poza nami
+        alter database Lab6db set single_user with rollback immediate;
+        drop database Lab6db;
+    end
+go
+
+create database Lab6db;
+go
+
+-- tworzymy nowego użytkownika w bazie "Lab6db"
+use [Lab6db];
+go
+
+if exists ( select name
+            from sys.database_principals
+            where name = 'Lab6user' )
+    begin
+        drop user [Lab6user];
+    end
+go
+
+create user [Lab6user] for login [Lab6user];
+go;
+
+-- przypisujemy użytkownika do roli
+alter role [db_owner] add member [Lab6user];
+go
