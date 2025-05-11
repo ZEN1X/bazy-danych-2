e95989383f88cd0201b27051d9a3d921b22f4ff1
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2025-05-12 00:30:43 +0200

    feat: prepare tables for task 2

--- lab10/lab10_prepare.sql
+++ lab10/lab10_prepare.sql
@@ -1,6 +1,7 @@
 use [testCLR];
 go
 
+-- Zadanie 1
 drop table if exists test1;
 go
 
@@ -20,5 +21,37 @@ create table test1_log
     log_date   datetimeoffset default sysdatetimeoffset() not null,
     user_login nvarchar(max)                              not null,
     data       nvarchar(max)                              not null
+);
+go
+
+-- Zadanie 2
+drop table if exists a;
+go
+
+create table a
+(
+    a_id int identity (1,1) primary key,
+    data nvarchar(100) not null
+);
+go
+
+drop table if exists b;
+go
+
+create table b
+(
+    b_id int identity (1,1) primary key,
+    data nvarchar(100) not null
+);
+go
+
+drop table if exists a_b;
+go
+
+create table a_b
+(
+    ab_a_id int not null foreign key references a (a_id),
+    ab_b_id int not null foreign key references b (b_id),
+    primary key (ab_a_id, ab_b_id)
 );
 go
