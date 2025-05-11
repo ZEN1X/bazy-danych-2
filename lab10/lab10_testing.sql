4fcdfb209124ed3ca3f71ca29164c2cf00564b3b
ZEN1X <20771421+ZEN1X@users.noreply.github.com>
2025-05-12 01:19:20 +0200

    feat: add task 2 solution to lab10

--- lab10/lab10_testing.sql
+++ lab10/lab10_testing.sql
@@ -1,6 +1,7 @@
 use [testCLR];
 go
 
+-- Zadanie 1
 insert into test1
     (data)
 values
@@ -10,4 +11,18 @@ go
 
 select *
 from test1_log;
+go
+
+-- Zadanie 2
+exec sp_InsertAandB @a_data = 'alpha', @b_data = 'beta';
+go
+
+select *
+from dbo.a;
+
+select *
+from dbo.b;
+
+select *
+from dbo.a_b;
 go
